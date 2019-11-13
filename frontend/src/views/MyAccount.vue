2260ba0ee3735da15031a080453b7eae090ba91f
KristLin <kristlin.unsw@gmail.com>
2019-11-13 13:49:29 +1100

    fix delete account bug

--- frontend/src/views/MyAccount.vue
+++ frontend/src/views/MyAccount.vue
@@ -136,9 +136,9 @@ export default {
               if (res.status == 200) {
                 this.$store.commit("logout");
                 window.console.log("user account is deleted");
-                window.location.reload(true);
                 this.$swal("Success", "Your account is deleted!", "success");
                 this.$router.push({ name: "home" });
+                window.location.reload(true);
               }
             })
             .catch(err => window.console.log(err));
